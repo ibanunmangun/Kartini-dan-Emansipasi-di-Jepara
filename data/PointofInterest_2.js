17607585997a49a11ae2d21c7e3bff64fab74581
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-04-11 19:43:06 +0700

    Add files via upload

---
+++ data/PointofInterest_2.js
@@ -0,0 +1,26 @@
+var json_PointofInterest_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Bandengan Beach", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.64872, -6.55436 ] } },
+{ "type": "Feature", "properties": { "Name": "Ikan Bakar Bu Gipah", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.65437, -6.55204 ] } },
+{ "type": "Feature", "properties": { "Name": "Museum R.A Kartini", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.66745, -6.58896 ] } },
+{ "type": "Feature", "properties": { "Name": "Pendopo Kartini Kabupaten Jepara", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.6678256, -6.5908514 ] } },
+{ "type": "Feature", "properties": { "Name": "Rengkot Buyut Restaurant", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.670604750234347, -6.594309762187512 ] } },
+{ "type": "Feature", "properties": { "Name": "Sembada Mebel Ukir Jepara", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.66188, -6.63755 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Sakit Graha Husada Jepara", "X": 110.670741, "Y": -6.593539, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.670741, -6.593539 ] } },
+{ "type": "Feature", "properties": { "Name": "Puskesmas Mantingan", "X": 110.670121, "Y": -6.617626, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.670121, -6.617626 ] } },
+{ "type": "Feature", "properties": { "Name": "RS PKU Aisyiyah Jepara", "X": 110.670967, "Y": -6.590801, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.670967, -6.590801 ] } },
+{ "type": "Feature", "properties": { "Name": "Pusat Kesehatan Desa (PKD) Petekeyan", "X": 110.666407, "Y": -6.631849, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.666407, -6.631849 ] } },
+{ "type": "Feature", "properties": { "Name": "Polres Jepara", "X": 110.666101, "Y": -6.596909, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.666101, -6.596909 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Mulyoharjo", "X": 110.678604, "Y": -6.584792, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.678604, -6.584792 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Krapyak", "X": 110.665146, "Y": -6.600743, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.665146, -6.600743 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Sukosono", "X": 110.66865, "Y": -6.635823, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.66865, -6.635823 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Agung Baitul Makmur", "X": 110.667374, "Y": -6.591117, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.667374, -6.591117 ] } },
+{ "type": "Feature", "properties": { "Name": "GPdI Calvary Chapel Jepara", "X": 110.666916, "Y": -6.589421, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.666916, -6.589421 ] } },
+{ "type": "Feature", "properties": { "Name": "Vihara Bhodi Jaya Manggala", "X": 110.665349, "Y": -6.589821, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.665349, -6.589821 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Agung Petekeyan", "X": 110.665259, "Y": -6.631806, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.665259, -6.631806 ] } },
+{ "type": "Feature", "properties": { "Name": "Area Parkir Pojok Alun-Alun Jepara", "X": 110.667162, "Y": -6.588854, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.667162, -6.588854 ] } }
+]
+}
